c93b9d252aacb37e9ef309f4d82782e6361be7e2
Parth Makadiya <parth.makadiya@hexad.de>
2018-04-12 21:34:14 +0200

    Initial Commit

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,7 +8,31 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+    var a=0;
+    func showToast(msg : String) {
+        
+        a=a+1;
+        var str = String(a);
+        let message = msg + ", " + str
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.textAlignment = .center;
+        toastLabel.font = UIFont(name: "Montserrat-Light", size: 12.0)
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +44,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func HelloWorldAction(_ sender: Any) {
+          self.showToast(msg: "Hello World")
+    }
+    
 }
 
